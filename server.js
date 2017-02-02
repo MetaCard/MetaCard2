3b5109399d35b54a6a9fb70a88acc3a5446f8e7a
Meegul <micha3lbilstein@gmail.com>
2017-02-02 14:21:34 -0500

    Comments and public files

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express();
 
 const port = 3000;
 
+//Enable pug as a template engine, from the views folder.
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+//Allow everything in ./public to be public.
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
